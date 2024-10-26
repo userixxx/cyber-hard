6eff14eb0aec53ef831f1429971a3fac1711bf2b
userix <romserenkov@gmail.com>
2024-10-26 05:06:37 +0300

    add(QuestionSeeder): A question sider has been added, and a migration bug has also been fixed

--- database/migrations/2024_10_26_014707_create_questions_table.php
+++ database/migrations/2024_10_26_014707_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->text('question'); // текст вопроса
-            $table->text('answer_')->nullable(); // ответ на вопрос
+            $table->text('answer')->nullable(); // ответ на вопрос
             $table->timestamps();
         });
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(UsersTableSeeder::class);
+        // TODO: доделать пользовательский сидр
+//        $this->call(UsersTableSeeder::class);
+        $this->call(QuestionSeeder::class);
     }
 }
